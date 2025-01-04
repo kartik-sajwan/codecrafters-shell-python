a716ba9002dbeb95e70de1a76b141030756499df
Kartik Sajwan <kartik.sajwan@gmail.com>
2025-01-05 03:43:13 +0530

    added Read-Eval-Print Loop

--- app/main.py
+++ app/main.py
@@ -2,13 +2,12 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+	while True:
+		sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    sys.stdout.write(f"{command}: command not found")
-    input()
+		# Wait for user input
+		command = input()
+		sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
