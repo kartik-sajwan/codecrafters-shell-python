986de024e02c072ee5d7b1e6667ea14050302d63
Kartik Sajwan <kartik.sajwan@gmail.com>
2025-01-05 04:33:33 +0530

    refactored code

--- app/main.py
+++ app/main.py
@@ -1,19 +1,32 @@
 import sys
 
 
+def echo(args):
+	echo_txt = " ".join(args)
+	sys.stdout.write(echo_txt + "\n")
+
+
+def exit(args):
+	sys.exit(0)
+
+
+builtin_commands = {
+	"echo": echo,
+	"exit": exit
+}
+
 def main():
 	while True:
-		sys.stdout.write("$ ")
+		# sys.stdout.write("$ ")
 
 		# Wait for user input
-		command = input()
+		user_input = input("$ ")
+		split_cmd_str = user_input.split(" ")
+		command = split_cmd_str[0]
+		args = split_cmd_str[1:]
 
-		if "exit" in command:
-			sys.exit(0)
-		elif "echo" in command:
-			split_str = command.split(" ")
-			echo_txt = " ".join(split_str[1:])
-			sys.stdout.write(echo_txt + "\n")
+		if command in builtin_commands:
+			builtin_commands[command](args)
 		else:
 			sys.stdout.write(f"{command}: command not found\n")
 
