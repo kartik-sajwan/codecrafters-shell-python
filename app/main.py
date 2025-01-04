44f977b8ed862c684838879090e32fe53e5e956a
Kartik Sajwan <kartik.sajwan@gmail.com>
2025-01-05 03:38:51 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
 
     # Wait for user input
     command = input()
-    sys.stdout.write(f"{command}: {command} not found")
+    sys.stdout.write(f"{command}: command not found")
     input()
 
 
